c03292339f2708cbc3b19b98d946c75ba333e1b5
rickycabugawan <rickycabugawan@totallylegit.edu.pl>
2025-06-10 05:56:22 +0800

    adding api sorting and search

--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -7,9 +7,21 @@ use Illuminate\Pagination\LengthAwarePaginator;
 
 class OrderService
 {
-    public function browse($page, $perPage, $sortBy, $sortOrder): LengthAwarePaginator
+    public function browse($page, $perPage, $sortBy, $sortOrder, $search): LengthAwarePaginator
     {
         $query = Order::query()->withCount('order_details');
+
+        if($search) {
+            $query->where('order_id', $search);
+        }
+
+        if($sortBy === 'item_count') {
+            $query->orderBy('order_details_count', $sortOrder);
+        } else if($sortBy === 'id') {
+            $query->orderBy('order_id', $sortOrder);
+        } else {
+            $query->orderBy($sortBy, $sortOrder);
+        }
 
         return $query->paginate($perPage, ['*'], 'page', $page);
     }
